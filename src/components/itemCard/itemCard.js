4ed1ed89f7e9fb399a8411bc1ec324f1eab1f211
Not Lil Saxx <lilsaxxo87@gmail.com>
2023-09-11 14:24:13 +0300

    fixing image size

--- src/components/itemCard/itemCard.js
+++ src/components/itemCard/itemCard.js
@@ -2,18 +2,17 @@ import react, { useState, useRef, useEffect } from "react";
 import "./itemCard.css";
 
 function ItemCard({ name, image }) {
-  const [imgSrc, setImageSrc] = useState(
-    `https://test.create.diagnal.com/images/${image}`
-  );
-
+  const [failedToGetImage, setFailedToGetImage] = useState(false);
   // in case of error while loading the image we will set default image url
   const handleImgError = () => {
-    setImageSrc('https://westsiderc.org/wp-content/uploads/2019/08/Image-Not-Available.png')
+    setFailedToGetImage(true);
   }
 
   return (
-    <div className="item-card">
-      <img onError={handleImgError} src={imgSrc} alt={name} />
+     <div className="item-card">
+      { failedToGetImage ? <div className="img-error-container">
+        <span>{`can't load image :(`}</span>
+      </div> : <img  onError={handleImgError} src={`https://test.create.diagnal.com/images/${image}`} alt={name} />}
       <h5> {name} </h5>
     </div>
   );
